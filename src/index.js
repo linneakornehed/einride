f29d494738d457b0093d5653b76141ee5390a1f6
Linnea Kornehed <linnea.kornehed@navisalma.se>
2017-03-20 21:46:40 +0100

    sm fix

--- src/index.js
+++ src/index.js
@@ -6,16 +6,14 @@ import Contact from './pages/Contact.jsx'
 import Investor from './pages/Investor.jsx'
 import About from './pages/About.jsx'
 import Home from './pages/Home.jsx'
-import { Router, Route, Link, IndexRoute, hashHistory, browserHistory, DefaultRoute, IndexLink } from 'react-router'
+import { Router, Route, Link, IndexRoute, hashHistory, IndexLink } from 'react-router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/css/bootstrap-theme.css'
 import './styles/main.css'
 
 ReactDOM.render(  
     <Router history={hashHistory}>
-        <IndexRoute component = {Home} />
         <Route path="/" component={App}/>
-        <Route path="/home" component={Home}/>
         <Route path="/about" component={About}/>
         <Route path="/jobs" component={Jobs}/>
         <Route path="/investor" component={Investor}/>
